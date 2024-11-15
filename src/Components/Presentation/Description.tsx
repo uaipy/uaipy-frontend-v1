af61d1e72a758a1f6366483e5569e6d4ba5f54f0
Ana Clara Custodio <anaccustodio8@gmail.com>
2024-11-15 10:31:54 -0300

    chore: att presentation

--- src/Components/Presentation/Description.tsx
+++ src/Components/Presentation/Description.tsx
@@ -2,16 +2,16 @@ export const Description = () => {
   return (
     <div className="space-y-4">
       <h1 className="text-center text-2xl font-bold">Descrição</h1>
-      <p className="text-center">
+      <p className="text-center mb-50">
         O projeto UAI.py combina tecnologias sustentáveis de baixo custo para
         gerenciar dados em ambientes distribuídos, oferecendo uma solução de IoT
         inovadora que melhora a usabilidade e escalabilidade de equipamentos
-        reciclados. <br />
-        Utilizando uma TVBox com a distribuição Armbian baseada em Linux, o
-        projeto desenvolve algoritmos para a comunicação e gerenciamento de
-        dados via serviços de nuvem, como Amazon Web Services (AWS). <br />
-        Os dados são medidos e processados localmente por sensores conectados ao
-        módulo ESP32-LoRa, que permite comunicação wireless a longa distância.
+        reciclados. Utilizando uma TVBox com a distribuição Armbian baseada em
+        Linux, o projeto desenvolve algoritmos para a comunicação e
+        gerenciamento de dados via serviços de nuvem, como Amazon Web Services
+        (AWS). Os dados são medidos e processados localmente por sensores
+        conectados ao módulo ESP32-LoRa, que permite comunicação wireless a
+        longa distância.
       </p>
     </div>
   );
